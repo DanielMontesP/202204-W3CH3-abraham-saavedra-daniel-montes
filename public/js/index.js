5488eff02626cb1eac333e58fd226ee8edb07fe2
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-20 21:20:55 +0200

    Add HTML card status

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ import series from "./series/series.js";
 import CardMovieComponent from "./componentes/CardMovieComponent.js";
 import Component from "./componentes/Component.js";
 
-debugger;
+//debugger;
 const container = document.querySelector(".container");
 const createCard = (item, index, arr) => {
   // arr[index] = item * 10;
-  new CardMovieComponent(container, item);
+  const newCard = new CardMovieComponent(container, item);
 };
 series.forEach(createCard);
