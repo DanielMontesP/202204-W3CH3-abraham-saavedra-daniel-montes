94729565b9ced187dc01886c0fecc4cd373da666
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-21 00:21:08 +0200

    Add siteUtils.js

--- public/js/siteUtils.js
+++ public/js/siteUtils.js
@@ -3,8 +3,7 @@ import series from "./series/series.js";
 const modArray = (arrayToMod, itemPos, propToMod, newValue) => {
   switch (arrayToMod) {
     case "series":
-      const serieToMod = series;
-      serieToMod.series[itemPos].watched = newValue;
+      series.series[itemPos].watched = newValue;
       break;
     default:
   }
