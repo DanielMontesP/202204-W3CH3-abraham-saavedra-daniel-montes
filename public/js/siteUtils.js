b59e13565346fee61c8c2afb2a832a43de621704
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-20 22:17:17 +0200

    Add HTML card status

--- public/js/siteUtils.js
+++
@@ -1,7 +0,0 @@
-const delItemArray = (arrayToMod, position) => {
-  // arrayToMod[position].(property) = newvalue};
-  // arrayToMod[position].borrado = true;
-  true=true;
-};
-
-export default delItemArray;
