343c60e4394e44363c5ff1ca2f2938750e35ef8e
Abraham Saavedra <abrasm92@gmail.com>
2022-04-21 21:45:20 +0200

    fix no new rule duplicated

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "no-new": "off",
     "import/extensions": "off",
     "no-plusplus": "off",
     "no-restricted-syntax": "off",
